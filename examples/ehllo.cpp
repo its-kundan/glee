f5b998c86091d36367730bfccf7ce66b6fb03f4b
Kundan <kundan51kk@gmail.com>
2023-09-23 19:39:33 +0530

    added a cpp file

---
+++ examples/ehllo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+// this is My codes library..
+using namespace std;
+int32_t main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL) ;
+    cout.tie(NULL);
+    long long t=1;
+    //cin>>t;
+    while(t--){
+    // long long n; cin>>n;
+   //vector<int> v; for(int i=0;i<n;i++){int x; cin>>x; v.pb(x);}
+        
+        cout<<"Hewllo kalkladsjfiajkjf";
+    }
+    return 0;
+}
